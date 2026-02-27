bdc7f6d897923ac2eff3d09549efa7b046de9c2f
Tori Champagne <champagnet@moravian.edu>
2026-02-27 16:04:51 -0500

    add sample docket

--- db/sample-data.sql
+++ db/sample-data.sql
@@ -43,3 +43,43 @@ INSERT INTO dockets (
     'CY 2026 Changes to the End-Stage Renal Disease (ESRD) Prospective Payment System and Quality Incentive Program.  CMS-1830-P',
     'This proposed rule would update and revise the End-Stage Renal Disease (ESRD) Prospective Payment System for calendar year 2026.  This rule also proposes to update the payment rate for renal dialysis services furnished by an ESRD facility to individuals with acute kidney injury.  In addition, this rule proposes to update requirements for the ESRD Quality Incentive Program and to terminate and modify requirements for the ESRD Treatment Choices Model.'
 );
+
+INSERT INTO dockets (
+    docket_id,
+    docket_api_link,
+    agency_id,
+    docket_category,
+    docket_type,
+    effective_date,
+    flex_field1,
+    flex_field2,
+    modify_date,
+    organization,
+    petition_nbr,
+    program,
+    rin,
+    short_title,
+    flex_subtype1,
+    flex_subtype2,
+    docket_title,
+    docket_abstract
+) VALUES (
+    'CMS-2025-0304',
+    'https://api.regulations.gov/v4/dockets/CMS-2025-0304',
+    'CMS',
+    NULL,
+    'Rulemaking',
+    NULL,
+    NULL,
+    NULL,
+    '2025-11-28T17:04:15Z',
+    NULL,
+    NULL,
+    NULL,
+    '0938-AV50',
+    NULL,
+    NULL,
+    NULL,
+    'CY 2026 Payment Policies under the Physician Fee Schedule and Other Changes to Part B Payment and Coverage Policies (CMS-1832-P)',
+    NULL
+);
